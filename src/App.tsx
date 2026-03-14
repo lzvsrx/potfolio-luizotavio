293d027443425767be49adc6056f65feec519bff
lzvsrx <valenzisousaluizotavio@outlook.com>
2026-03-14 17:23:49 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import Header from "./components/Header"
+import About from "./components/About"
+import Skills from "./components/Skills"
+import Projects from "./components/Projects"
+import Footer from "./components/Footer"
+
+import "./styles/global.css"
+
+function App() {
+
+return (
+
+<>
+<Header />
+
+<div className="container">
+
+<About />
+
+<Skills />
+
+<Projects />
+
+</div>
+
+<Footer />
+
+</>
+
+)
+
+}
+
+export default App
